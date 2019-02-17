05e26a42c51e4a56239e3b048530350e1c0ab3db
Matthew Soulanille <matthew@soulanille.net>
2019-02-17 16:56:46 -0500

    Disable tslint

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ let mainWindow: Electron.BrowserWindow;
 
 ElectronApp.on('ready', () => {
 	mainWindow = new BrowserWindow({ width: 800, height: 600, resizable: false });
-	mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+	mainWindow.loadURL("file://" + __dirname + "/app/index.html");
 	mainWindow.webContents.openDevTools();
 	mainWindow.on('closed', () => {
 		mainWindow = null;
