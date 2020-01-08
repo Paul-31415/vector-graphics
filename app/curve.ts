6232993382de5c38960929555f45e865bd7633c1
Paul Soulanille <paul@soulanille.net>
2020-01-07 16:38:07 -0800

    before attempting to get working build with three.js

--- app/curve.ts
+++ app/curve.ts
@@ -1,7 +1,7 @@
 import { Saveable } from "./save";
 import { Vector } from "./vector";
 
-interface Curve<T> extends Saveable {
+interface Curve<T> {
     get(t: number): T;
     derivative(): Curve<T>;
 }
@@ -11,7 +11,469 @@ function py_mod(n: number, m = 1): number {
     return ((n % m) + m) % m;
 }
 
+@Saveable.register
+class NURBS<T extends Vector<any>> extends Vector<NURBS<T>> implements Curve<T>, Saveable {
+    _saveName?: string;
+    public points: Array<{ weight: number, value: T }>;
+    public knots: Array<number>;
+    public degree: number;
+    public cyclic: boolean;
+    constructor(pts: Array<T> | null = null, degree = 0, weights: Array<number> | null = null, knots: Array<number> | null = null, cyclic = false) {
+        super();
+        if (pts != null) {
+            this.degree = degree;
+            this.cyclic = cyclic;
+            if (knots == null) {
+                this.knots = [];
+                for (let i = 0; i < 1 + degree + pts.length; i += 1) {
+                    this.knots[i] = i + 1;
+                }
+            } else {
+                this.knots = knots;
+            }
+            this.points = []
+            if (weights == null) {
+                for (var i in pts) {
+                    this.points[i] = { weight: 1, value: pts[i] };
+                }
+            } else {
+                for (var i in pts) {
+                    this.points[i] = { weight: weights[i], value: pts[i] };
+                }
+            }
+        }
+    }
+    //::::Vector::::
+    set(o: NURBS<T>): NURBS<T> {
+        this.points = o.points;
+        this.knots = o.knots;
+        this.degree = o.degree;
+        this.cyclic = o.cyclic;
+        return this;
+    }
+    copy(): NURBS<T> {
+        return new NURBS<T>().set(this);
+    }
+    scale(s: number): NURBS<T> {
+        const r = new NURBS<T>();
+        for (var i in this.points) {
+            r.points[i] = { weight: this.points[i].weight * s, value: this.points[i].value.scale(s) };
+        }
+        r.knots = this.knots;
+        r.degree = this.degree;
+        r.cyclic = this.cyclic;
+        return r;
+    }
+    add(o: NURBS<T>): NURBS<T> {
+        throw o;
+    }
 
+
+
+    //::::Curve::::
+    get(x: number) {
+        if (this.cyclic) {
+            x = py_mod(x, this.knots[this.knots.length - 1]);
+        }
+        //calc weights
+        const i = this.getKnotOfInterest(x) - 1;
+
+        const w = [1];//b(i=t,0,x)
+        for (var k = 1; k < this.degree; k += 1) {
+
+            const n = x - this.knots[i];
+            const d = this.knots[i + k] - this.knots[i];
+            if (d == 0) {
+                w.push(w[w.length - 1]);
+            } else {
+                w.push(w[w.length - 1] * n / d);
+            }
+            for (var ioff = -1; ioff >= -w.length; ioff -= 1) {
+                const cfs = this.b_func_cofactor(i + ioff, k, x);
+                w[w.length + ioff] = w[w.length + ioff] * cfs[0] + w[w.length + ioff - 1] * cfs[1];
+            }
+            const n2 = this.knots[i + k + 2 - w.length] - x;
+            const d2 = this.knots[i + k + 2 - w.length] - this.knots[i + 2 - w.length];
+            if (d2 != 0) {
+                w[0] *= n2 / d2;
+            }
+        }
+
+        const resP = this.points[i].value.scale(this.points[i].weight * w[w.length - 1]);
+        var resW = this.points[i].weight * w[w.length - 1];
+        for (var wi = -2; wi >= -w.length; wi -= 1) {
+            resP.addDEq(this.points[i + wi].value.scale(this.points[i + wi].weight * w[w.length + wi]));
+            resW += this.points[i + wi].weight * w[w.length + wi];
+        }
+        return resP.scaleEq(1 / resW);
+    }
+    derivative(): NURBS<T> {
+
+        var cps = new Array<T>();
+        var cwd = new Array<number>();
+        for (var i = 0; i < this.points.length - 1; i++) {
+            const f = this.degree / (this.knots[i + this.degree] - this.knots[i]);
+            cwd[i] = (this.points[i + 1].weight - this.points[i].weight) * f;
+            cps[i] = this.points[i + 1].value.sub(this.points[i].value).scaleEq(f);
+        }
+        throw this;
+        return new NURBS(cps, this.degree - 1, cwd, [...this.knots], this.cyclic);
+    }
+
+
+
+
+
+    //::::helper/misc::::
+
+    getKnotOfInterest(t: number): number {
+
+        //returns least i for knot[i] ≥ t
+        let l = 0;
+        let h = this.knots.length - 1;
+        while (l < h) {
+            const m = Math.floor((l + h) / 2);
+            const v = this.knots[m];
+            if (t > v) {
+                l = m;
+            } else {
+                if (t < v) {
+                    h = m;
+                } else {
+                    break;
+                }
+            }
+        }
+        while (l > 0 && this.knots[l] >= t) {
+            l -= 1;
+        }
+        return l;
+    }
+
+
+    b_func_cofactor(i: number, k: number, x: number): number[] {
+        //b(i,k,x) = (x-t(i))/(t(i+k)-t(i)) * b(i,k-1,x) + (t(i+k+1)-x)/(t(i+k+1)-t(i+1)) * b(i+1,k-1,x)
+        const r = [x - this.knots[i], this.knots[i + k + 1] - x];
+        const d1 = this.knots[i + k] - this.knots[i];
+        if (d1 == 0) {
+            r[0] = 1;
+        } else {
+            r[0] /= d1;
+        }
+        const d2 = this.knots[i + k + 1] - this.knots[i + 1];
+        if (d2 == 0) {
+            r[1] = 1;
+        } else {
+            r[1] /= d2;
+        }
+        return r;
+    }
+
+}
+
+@Saveable.register
+class NUBS<T extends Vector<any>> extends Vector<NUBS<T>> implements Curve<T>, Saveable {
+    _saveName?: string;
+    public points: Array<T>;
+    public knots: Array<number>;
+    public degree: number;
+    public cyclic: boolean;
+    constructor(pts: Array<T> | null = null, degree = 0, knots: Array<number> | null = null, cyclic = false) {
+        super();
+        if (pts != null) {
+            this.degree = degree;
+            this.cyclic = cyclic;
+            if (knots == null) {
+                this.knots = [];
+                for (let i = 0; i < 1 + degree + pts.length; i += 1) {
+                    this.knots[i] = i + 1;
+                }
+            } else {
+                this.knots = knots;
+            }
+            this.points = pts;
+        }
+    }
+    //::::Tower::::
+    NURBS(): NURBS<T> {
+        return new NURBS<T>(this.points, this.degree, null, this.knots, this.cyclic);
+    }
+    //::::Vector::::
+    set(o: NUBS<T>): NUBS<T> {
+        this.points = o.points;
+        this.knots = o.knots;
+        this.degree = o.degree;
+        this.cyclic = o.cyclic;
+        return this;
+    }
+    copy(): NUBS<T> {
+        return new NUBS<T>().set(this);
+    }
+    scale(s: number): NUBS<T> {
+        const r = new NUBS<T>();
+        for (var i in this.points) {
+            r.points[i] = this.points[i].scale(s);
+        }
+        r.knots = this.knots;
+        r.degree = this.degree;
+        r.cyclic = this.cyclic;
+        return r;
+    }
+    add(o: NUBS<T>): NUBS<T> {
+        throw o;
+    }
+
+
+
+    //::::Curve::::
+    get(x: number) {
+        if (this.cyclic) {
+            x = py_mod(x, this.knots[this.knots.length - 1]);
+        }
+        //calc weights
+        const i = this.getKnotOfInterest(x) - 1;
+
+        const w = [1];//b(i=t,0,x)
+        for (var k = 1; k < this.degree; k += 1) {
+
+            const n = x - this.knots[i];
+            const d = this.knots[i + k] - this.knots[i];
+            if (d == 0) {
+                w.push(w[w.length - 1]);
+            } else {
+                w.push(w[w.length - 1] * n / d);
+            }
+            for (var ioff = -1; ioff >= -w.length; ioff -= 1) {
+                const cfs = this.b_func_cofactor(i + ioff, k, x);
+                w[w.length + ioff] = w[w.length + ioff] * cfs[0] + w[w.length + ioff - 1] * cfs[1];
+            }
+            const n2 = this.knots[i + k + 2 - w.length] - x;
+            const d2 = this.knots[i + k + 2 - w.length] - this.knots[i + 2 - w.length];
+            if (d2 != 0) {
+                w[0] *= n2 / d2;
+            }
+        }
+
+        const res = this.points[i].scale(w[w.length - 1]);
+        for (var wi = -2; wi >= -w.length; wi -= 1) {
+            res.addDEq(this.points[i + wi].scale(w[w.length + wi]));
+        }
+        return res;
+    }
+    derivative(): NUBS<T> {
+
+        var cps = new Array<T>();
+        for (var i = 0; i < this.points.length - 1; i++) {
+            const f = this.degree / (this.knots[i + this.degree] - this.knots[i]);
+            cps[i] = this.points[i + 1].sub(this.points[i]).scaleEq(f);
+        }
+        return new NUBS(cps, this.degree - 1, [...this.knots], this.cyclic);
+    }
+
+
+
+
+
+    //::::helper/misc::::
+
+    getKnotOfInterest(t: number): number {
+
+        //returns least i for knot[i] ≥ t
+        let l = 0;
+        let h = this.knots.length - 1;
+        while (l < h) {
+            const m = Math.floor((l + h) / 2);
+            const v = this.knots[m];
+            if (t > v) {
+                l = m;
+            } else {
+                if (t < v) {
+                    h = m;
+                } else {
+                    break;
+                }
+            }
+        }
+        while (l > 0 && this.knots[l] >= t) {
+            l -= 1;
+        }
+        return l;
+    }
+
+
+    b_func_cofactor(i: number, k: number, x: number): number[] {
+        //b(i,k,x) = (x-t(i))/(t(i+k)-t(i)) * b(i,k-1,x) + (t(i+k+1)-x)/(t(i+k+1)-t(i+1)) * b(i+1,k-1,x)
+        const r = [x - this.knots[i], this.knots[i + k + 1] - x];
+        const d1 = this.knots[i + k] - this.knots[i];
+        if (d1 == 0) {
+            r[0] = 1;
+        } else {
+            r[0] /= d1;
+        }
+        const d2 = this.knots[i + k + 1] - this.knots[i + 1];
+        if (d2 == 0) {
+            r[1] = 1;
+        } else {
+            r[1] /= d2;
+        }
+        return r;
+    }
+
+}
+
+
+@Saveable.register
+class Bezier<T extends Vector<any>> extends Vector<Bezier<T>> implements Curve<T>, Saveable {
+    _saveName?: string;
+    constructor(public points: Array<T> = []) {
+        super();
+    }
+    //::::Tower::::
+    NURBS(): NURBS<T> {
+        return this.NUBS().NURBS();
+    }
+    NUBS(): NUBS<T> {
+        const knots = []
+        for (let i = 0; i < 1 + this.points.length * 2; i += 1) {
+            knots[i] = (i >= this.points.length) ? 1 : 0;
+        }
+        return new NUBS<T>(this.points, this.points.length, knots);
+    }
+    //::::Vector::::
+    set(o: Bezier<T>): Bezier<T> {
+        this.points = o.points;
+        return this;
+    }
+    copy(): Bezier<T> {
+        const cp: T[] = [];
+        for (var i in this.points) {
+            cp[i] = this.points[i].copy();
+        }
+        return new Bezier<T>(cp);
+    }
+    scale(s: number): Bezier<T> {
+        const r = new Bezier<T>();
+        for (var i in this.points) {
+            r.points[i] = this.points[i].scale(s);
+        }
+        return r;
+    }
+    add(o: Bezier<T>): Bezier<T> {
+        throw o;
+    }
+
+
+    //::::Curve::::
+    get(t: number): T {
+        if (this.order == 0) {
+            return this.points[0].copy().get();
+        }
+
+        var res = this.points[0].scale(
+            Math.pow((1 - t), this.order)
+        );
+        var bin = this.order;
+        for (var i = 1; i < this.points.length; i++) {
+            res.addEqDiscardOther(this.points[i].scale(
+                bin *
+                Math.pow((1 - t), this.order - i) *
+                Math.pow(t, i)
+            ));
+            bin *= (this.order - i) / (i + 1);
+        }
+        return res;
+    }
+    derivative(): Bezier<T> {
+        var cps: T[] = [];
+        if (this.order == 0) {
+            return new Bezier<T>([this.points[0].scale(0)]);
+        }
+        for (var i = 0; i < this.order; i++) {
+            cps[i] = this.points[i + 1].sub(this.points[i]).scaleEq(this.degree);
+        }
+        return new Bezier<T>(cps);
+    }
+
+
+    //::::helper/misc::::
+    get order(): number {
+        return this.points.length - 1;
+    }
+    get degree(): number {
+        return this.points.length;
+    }
+
+
+    deCastlejau(t: number): T[][] {
+        var ans: T[][] = [this.points];
+        while (ans[ans.length - 1].length > 1) {
+            ans.push([]);
+            for (var i = 0; i < ans[ans.length - 2].length - 1; i++) {
+                ans[ans.length - 1].push(ans[ans.length - 2][i + 1].scale(t).addDEq(ans[ans.length - 2][i].scale(1 - t)));
+            }
+        }
+        return ans;
+    }
+    split(t: number): Bezier<T>[] {
+        const c = this.copy();
+        return [c, c.splitEq(t)];
+    }
+    splitEq(t: number): Bezier<T> {
+        const o: T[] = [];
+        for (var j = 0; j < this.points.length; j++) {
+            o[this.points.length - 1 - j] = this.points[this.points.length - 1].copy();
+            for (var i = this.points.length - 1; i > j; i--) {
+                this.points[i].scaleEq(t).addDEq(this.points[i - 1].scale(1 - t));
+            }
+        }
+        return new Bezier<T>(o);
+    }
+    cutL(t: number): Bezier<T> {
+        return this.copy().cutLEq(t);
+    }
+    cutLEq(t: number): Bezier<T> {
+        for (var j = 0; j < this.points.length; j++) {
+            for (var i = this.points.length - 1; i > j; i--) {
+                this.points[i].scaleEq(t).addDEq(this.points[i - 1].scale(1 - t));
+            }
+        }
+        return this;
+    }
+    cutG(t: number): Bezier<T> {
+        return this.copy().cutGEq(t);
+    }
+    cutGEq(t: number): Bezier<T> {
+        return this.reverseEq().cutLEq(1 - t).reverseEq();
+    }
+    reverse(): Bezier<T> {
+        return this.copy().reverseEq();
+    }
+    reverseEq(): Bezier<T> {
+        this.points.reverse();
+        return this;
+    }
+
+    incOrderEq(): Bezier<T> {
+        const n = this.order;
+        if (n == 0) {
+            this.points[1] = this.points[0].copy();
+            return this;
+        }
+        const k = n + 1;
+        this.points[k] = this.points[n].copy();
+        for (var i = n; i >= 1; i--) {
+            this.points[i].scaleEq((n - i + 1) / (i + 1)).addEq(this.points[i - 1].scaleEq(i / k));
+        }
+        return this;
+    }
+
+
+
+
+
+
+
+}
 
 
 
@@ -74,7 +536,7 @@ class Spline<T extends Vector<any>> extends Vector<Spline<T>> implements Curve<T
         var start = this.getPointIndex(t);
         const weights: number[] = [1];
 
-        // determining of order of section
+        // determining of degree of section
 
 
 
--- app/index.ts
+++ app/index.ts
@@ -1,23 +1,17 @@
 import * as PIXI from "pixi.js";
-import { Pen, SmartPen, Brusher, BrushedCurve, Dots, PerspPen, DebugPen } from "./brush";
-import { PtTransform, Transform } from "./transform";
-import { Bezier, WeightedVector, BSpline, WeightedAverageCurve, Spline } from "./bezier";
-import { Point, Style, Color, Scalar } from "./vectors";
-import { Canvas } from "./canvas";
-import { Graphics } from "pixi.js";
-import { BezTool, BSplineTool } from "./tools";
-import { randomBytes } from "crypto";
-import { Acceptor } from "./toolInterfaces";
-import { save, load, download, asyncLoad, asyncSave, TreeProgressBar, AsyncTreeProcess, BiMap, Saveable } from "./save";
-import { State } from "./state";
-import { Graphic } from "./drawable";
-import { PerspCam } from "./camera";
-import { Angle } from "./angle";
-import * as GPU from "gpu.js";
-const gpu = new GPU.GPU();
+import * as THREE from "three";
+
+let three_scene = new THREE.Scene();
+let three_camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let three_renderer = new THREE.WebGLRenderer();
+three_renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(three_renderer.domElement);
 
 
-let renderer = PIXI.autoDetectRenderer();
+
+
+
+let pixi_renderer = PIXI.autoDetectRenderer();
 
 const app: PIXI.Application = new PIXI.Application(
     {
@@ -32,293 +26,19 @@ const app: PIXI.Application = new PIXI.Application(
 var saveDirectory = "./saves/";
 
 
-var state: State = new State();
-
-
-function color(r = 0, g = 0, b = 0): Style {
-    return new Style({ "color": new Color(r, g, b, 1), "width": new Scalar(2) });
-}
+//document.body.appendChild(app.view);
 
 
 
-
-
-
-/*/hackey stuff now
-var DistFunc = function(pos: number[]): number {
-    return Math.sqrt(pos[0] ** 2 + pos[1] ** 2 + pos[2] ** 2) - 1;
-}
-
-
-var rayMarchingKernalMake = gpu.createKernel(function(a: number[][]) {
-    if (this.thread.z >= 3) {
-        const x = (a[1][0] + a[2][0] * this.thread.x + a[3][0] * this.thread.y);
-        const y = (a[1][1] + a[2][1] * this.thread.x + a[3][1] * this.thread.y);
-        const z = (a[1][2] + a[2][2] * this.thread.x + a[3][2] * this.thread.y);
-        const mag = Math.sqrt(x * x + y * y + z * z);
-        switch (this.thread.z) {
-            case 3:
-                return x / mag;
-            case 4:
-                return y / mag;
-            case 5:
-                return z / mag;
-        }
-    } else {
-        switch (this.thread.z) {
-            case 0:
-                return a[0][0];
-            case 1:
-                return a[0][1];
-            case 2:
-                return a[0][2];
-        }
-    }
-}).setOutput([512, 512, 6]);
-
-var rayMarchingKernal = gpu.createKernel(function(a: number[][][], steps: number) {
-    for (var r = 0; r < steps; r++) {
-        const dist = DistFunc(a[this.thread.x][this.thread.y]);
-        for (var i = 0; i < 3; i++) {
-            a[this.thread.x][this.thread.y][i] += a[this.thread.x][this.thread.y][i + 3] * dist;
-        }
-    }
-    return a[this.thread.x][this.thread.y][this.thread.z];
-}).setOutput([512, 512, 6]).setFunctions([DistFunc]);
-
-var rayMarchingKernalRender = gpu.createKernel(function(a: number[][][]) {
-    const dist = DistFunc(a[this.thread.x][this.thread.y]);
-    this.color(255 / dist, 255 / dist, 255 / dist, 1);
-}).setOutput([512, 512]).setGraphical(true);
-
-
-
-
-*///hackey stuff end
-
-
-document.body.appendChild(app.view);
-class Id<A> implements Transform<A, A> {
-    apply(a: A): A {
-        return a;
-    }
-    transform_linear = true;
-    transform_invertible = true;
-    inverse(): Transform<A, A> {
-        return this;
-    }
-    unapply(b: A): A {
-        return b;
-    }
-    _saveName?: string;
-}
-var id = new Id<Point>();
-
-@Saveable.register
-class inversePerspTrns implements Transform<Point, Point>{
-    transform_linear = false;// affine
-    transform_invertible = true;
-    inverse(): Transform<Point, Point> {
-        throw new Error("no");
-    }
-    unapply(a: Point): Point {
-        return this.t.apply(a).addEqDiscardOther(new Point(0, 0, -this.z)).scaleEq(1 / this.s);
-    }
-    _saveName?: string;
-    constructor(public t: Transform<Point, Point>, public z = 1, public s = 1 / 512, public defW = 16 / 512) { }
-    apply(a: Point): Point {
-        const aa = a.copy();
-        aa.addEqDiscardOther(new Point(-window.innerWidth / 2, -window.innerHeight / 2));
-        aa.y *= -1;
-        const r = this.t.unapply(aa.scale(this.s).addEqDiscardOther(new Point(0, 0, this.z)));
-        if (r.s.vars.width == null) {
-            r.s.vars.width = new Scalar(this.defW);
-        }
-        return r;
-    }
-
-}
-
-
-
-var view = new PIXI.Container();
-
-
-//view.scale.set(state.viewPortScale, -state.viewPortScale);
-
-app.stage.addChild(view);
-const g = new PIXI.Graphics();
-g.moveTo(0, 0);
-g.beginFill(0);
-g.lineTo(0, 1000);
-g.lineTo(1000, 1000);
-g.lineTo(1000, 0);
-g.endFill();
-g.alpha = 0;
-const txt = renderer.generateTexture(g);
-
-const br = new BSplineTool(new Brusher(state.canvas, new DebugPen(color(1, 1, 0)), 128), 3, txt, new inversePerspTrns(state.viewCam.val));
-
-
-
-
-
-
-
-app.stage.addChild(br);
-app.stage.addChild(view);
+//app.stage.addChild(view);
 
 window.onresize = function(_event: UIEvent): void {
-    app.renderer.resize(window.innerWidth, window.innerHeight);
-    view.x = window.innerWidth / 2;
-    view.y = window.innerHeight / 2;
+    //app.renderer.resize(window.innerWidth, window.innerHeight);
+
 
 
 };
 
+//app.stage.addChild(fpsMeter);
 
-
-state.attach(document);
-
-
-function renderView() {
-
-    /* const o = state.viewCam.val.position;
-     const forward = state.viewCam.val.rotation.apply(new Point(0, 0, 1));
-     const up = state.viewCam.val.rotation.apply(new Point(0, 1, 0)).scaleEq(1 / 256);
-     const sideways = state.viewCam.val.rotation.apply(new Point(1, 0, 0)).scaleEq(1 / 256);
-     forward.addEq(up.scale(256));
-     forward.addEq(sideways.scale(256));
- 
- 
-     const rays = rayMarchingKernal(rayMarchingKernalMake([[o.getX(), o.getY(), o.getZ()], [forward.getX(), forward.getY(), forward.getZ()], [up.getX(), up.getY(), up.getZ()], [sideways.getX(), sideways.getY(), sideways.getZ()]]), 10);
- 
-     rayMarchingKernalRender(rays);
- 
- 
-     const marchCvs = rayMarchingKernalRender.canvas;
-     app.stage.addChild(marchCvs);
-     */
-    //const i = app.stage.getChildIndex(view);
-    //app.stage.removeChildAt(i);
-    //view = state.renderView(window);
-
-    //3d support:
-    view.removeChildren();
-    const s = state.renderView(window);
-    view.addChild(s);
-    //s.filters = [new PIXI.filters.BlurFilter()];
-    //state.rerenderView(view);
-    view.x = window.innerWidth / 2;
-    view.y = window.innerHeight / 2;
-
-
-    //app.stage.addChildAt(view, i);
-    barGraphics.scale.set(window.innerWidth, window.innerHeight);
-}
-
-
-
-
-const fpsMeter = new PIXI.Text("FPS:__");
-
-fpsMeter.y = 20;
-fpsMeter.x = 20;
-
-
-app.stage.addChild(fpsMeter);
-
-
-
-var time = 0;
-var fps = 0;
-var fpsa = 0.1;
-
-
-var loading = false;
-
-var bar: TreeProgressBar = null;
-const barGraphics = new PIXI.Graphics();
-app.stage.addChild(barGraphics);
-
-function setState(s: State) {
-    state.detach();
-    state = s;
-    state.attach(document);
-
-    br.inputTransform = state.viewCam.val;
-    (br.target as Brusher).dest = state.canvas;
-}
-
-
-
-import * as fs from 'fs';
-try {
-
-    var data = fs.readFileSync(saveDirectory + 'input.json', "utf8");
-    const cb = function(d: any) {
-        setState((d as AsyncTreeProcess<any>).result as State);
-    }
-    asyncLoad(data, cb);
-} catch (e) {
-    console.log("file not loaded");
-    console.log(e);
-}
-
-
-
-function physLoop(delta: number): void {
-    if (loading) {
-        barGraphics.clear();
-        const p = bar.getProgress();
-        barGraphics.lineStyle(1 / 32, 0x010000 * Math.floor((1 - p) * 0xff) + 0x000100 * Math.floor(p * 0xff), 1, 0.5);
-        barGraphics.moveTo(0, 0);
-        barGraphics.lineTo(p, 0);
-        barGraphics.lineStyle(1 / 64, 0x880000, 1, 0.5);
-        for (var i = 0; i < bar.denominators.length; i++) {
-            barGraphics.moveTo(0, (i + 1) / 32);
-            barGraphics.lineTo(bar.numerators[i] / bar.denominators[i], (i + 1) / 32);
-        }
-
-    } else {
-        renderView();
-        time += delta;
-        fps = fps * (1 - fpsa) + (60 / delta) * fpsa;
-        fpsMeter.text = "FPS:" + fps;
-        state.physLoop(delta / 60);
-    }
-}
-app.ticker.add(physLoop);
-
-
-
-
-document.addEventListener("keydown", (ke: KeyboardEvent) => {
-
-    console.log(ke);
-    if (ke.code == "Escape") {
-        state.freecam = !state.freecam;
-    }
-});
-
-
-
-
-//save
-document.addEventListener("keydown", (ke: KeyboardEvent) => {
-    if (ke.code == "KeyS" && ke.metaKey) { //command-s
-        const cb = function(o: AsyncTreeProcess<string>) {
-            download(saveDirectory + state._saveName, o.result);
-            loading = false;
-            bar = null;
-            barGraphics.clear();
-        }
-        loading = true;
-        const pros = asyncSave(state, new BiMap<string, any>(), cb);
-        //pop up a loading screen/dialogue with a progress bar and cancel button etc.
-        //todo
-        bar = pros.progress;
-        console.log(pros);
-
-    }
-});
+//app.ticker.add(physLoop);
