a6107b913f30aee18b4e4cbf57b585a148003ba5
Paul Soulanille <paul@soulanille.net>
2020-01-07 17:26:54 -0800

    at least it seems to compile

--- app/index.ts
+++ app/index.ts
@@ -1,12 +1,22 @@
 import * as PIXI from "pixi.js";
 import * as THREE from "three";
 
+
+//from getting started in the docs
 let three_scene = new THREE.Scene();
 let three_camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 let three_renderer = new THREE.WebGLRenderer();
 three_renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(three_renderer.domElement);
 
+
+//cube from docs
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+three_scene.add(cube);
+
+three_camera.position.z = 5;
 
 
 
@@ -34,7 +44,7 @@ var saveDirectory = "./saves/";
 
 window.onresize = function(_event: UIEvent): void {
     //app.renderer.resize(window.innerWidth, window.innerHeight);
-
+    three_renderer.setSize(window.innerWidth, window.innerHeight);
 
 
 };
@@ -42,3 +52,10 @@ window.onresize = function(_event: UIEvent): void {
 //app.stage.addChild(fpsMeter);
 
 //app.ticker.add(physLoop);
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    three_renderer.render(three_scene, three_camera);
+}
+animate();
